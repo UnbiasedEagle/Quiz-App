fc2a2c4a3440b96824192d862f51e0af5c674462
Saurabh Singh <imsaurabhsingh007@gmail.com>
2019-10-02 20:56:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let correctAnswers=['B','A','A','A'];
+const form=document.querySelector(".quiz-form")
+form.addEventListener('submit',e=>{
+    e.preventDefault();
+    let score=0;
+    const userAnswers=[form.q1.value,form.q2.value,form.q3.value,form.q4.value];
+
+    //checkAnswer
+    userAnswers.forEach((answer,index)=>{
+        if(answer===correctAnswers[index]){
+            score+=25;
+        }
+    });
+    scrollTo(0,0);
+    const result=document.querySelector('.result');
+    result.classList.remove('d-none');
+    const mark=document.querySelector('.score');
+    let i=0;
+    const timer=setInterval(()=>{
+        if(i<score){
+            i++;
+            mark.innerHTML=i+"%";
+        }
+        else{
+            mark.innerHTML=0+'%';
+        }
+    },5);
+});
+
